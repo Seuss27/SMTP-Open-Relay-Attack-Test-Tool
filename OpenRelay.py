547c1c1c97b74e70e1b6e62b0c049a6cebaaa2a0
Jared Groves <jared.groves@philips.com>
2020-08-04 16:05:25 -0400

    Updated to work with Python 3

--- OpenRelay.py
+++ OpenRelay.py
@@ -1,35 +1,34 @@
 import socket
 import smtplib
-from smtplib import *
 
-IP = raw_input("Enter Ip address: ")
-Port = raw_input("Enter Port Number: ")
+IP = input("Enter IP address: ")
+Port = input("Enter Port Number: ")
 
-X = raw_input("From: ")
-Y = raw_input("TO: ")
+X = input("From: ")
+Y = input("TO: ")
 
 s = socket.socket()
-s.connect((IP,int(Port)))
+s.connect((IP, int(Port)))
 socket.setdefaulttimeout(3)
 
 ans = s.recv(1024)
 
 
-if ("220" in ans):
-    print "\n[+]Port" + " " + str(Port) + " " + "open on the target system\n"
-    smtpserver = smtplib.SMTP(IP,int(Port))
-    r = smtpserver.docmd("Mail From:",X)
+if '220' in str(ans):
+    print("\n[+]Port {} open on the target system\n".format(Port))
+    smtpserver = smtplib.SMTP(IP, int(Port))
+    r = smtpserver.docmd("Mail From:", X)
     a = str(r)
-    if ("250" in a):
-        r = smtpserver.docmd("RCPT TO:",Y)
+    if "250" in a:
+        r = smtpserver.docmd("RCPT TO:", Y)
         a = str(r)
-        if ("250" in a):
+        if "250" in a:
             
-            print "[+]The target system seems vulenarble to Open relay attack"
+            print("[+]The target system seems vulenarble to Open relay attack")
 
         else:
-            print "[-]The target system is not vulnerable to Open relay attack "
+            print("[-]The target system is not vulnerable to Open relay attack")
         
     
 else:
-    print "[-]Port is closed/Filtered"
+    print("[-]Port is closed/Filtered")
