1d188fb321d198c28a98e7be1c632180c73f19c9
Jared Groves <jared.groves@philips.com>
2020-08-04 16:24:30 -0400

    Broke out tests to individual functions

--- OpenRelay.py
+++ OpenRelay.py
@@ -7,15 +7,21 @@ Port = input("Enter Port Number: ")
 X = input("From: ")
 Y = input("TO: ")
 
-s = socket.socket()
-s.connect((IP, int(Port)))
-socket.setdefaulttimeout(3)
 
-ans = s.recv(1024)
+def smtp_connect(ip, port):
+    s = socket.socket()
+    s.connect((ip, int(port)))
+    socket.setdefaulttimeout(3)
+
+    return s.recv(1024)
 
 
-if '220' in str(ans):
-    print("\n[+]Port {} open on the target system\n".format(Port))
+def is_open(answer):
+    if '220' in str(answer):
+        return True
+
+
+def test_relay(ip, port):
     smtpserver = smtplib.SMTP(IP, int(Port))
     r = smtpserver.docmd("Mail From:", X)
     a = str(r)
@@ -23,12 +29,18 @@ if '220' in str(ans):
         r = smtpserver.docmd("RCPT TO:", Y)
         a = str(r)
         if "250" in a:
-            
-            print("[+]The target system seems vulenarble to Open relay attack")
-
+            return True
         else:
-            print("[-]The target system is not vulnerable to Open relay attack")
-        
-    
+            return False
+
+
+ans = smtp_connect(IP, Port)
+
+if is_open(ans):
+    print("\n[+]Port {} open on the target system\n".format(Port))
+    if test_relay(IP, Port):
+        print("[+]The target system seems vulenarble to Open relay attack")
+    else:
+        print("[-]The target system is not vulnerable to Open relay attack")
 else:
     print("[-]Port is closed/Filtered")
